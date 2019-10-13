92dc1c43b5f962c10f056cd0144803dd9ab4e7a0
Georges Savoundararadj <savoundg@gmail.com>
2019-10-13 09:45:28 -0700

    mw_backup_main.sh: fix error message

--- mw_backup_main.sh
+++ mw_backup_main.sh
@@ -3,6 +3,6 @@
 . /usr/local/bin/mw_backup.sh
 
 [ -z "$MW_ROOT" ] && echo "MW_ROOT is not defined" && exit 1
-[ -z "$MW_BACKUP_OUTPUT" ] && echo "MW_BACKUP_OUTPUT" && exit 2
+[ -z "$MW_BACKUP_OUTPUT" ] && echo "MW_BACKUP_OUTPUT is not defined" && exit 2
 
 mw_backup "$MW_ROOT" "$MW_BACKUP_OUTPUT"
